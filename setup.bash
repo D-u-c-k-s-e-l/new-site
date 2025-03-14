0de141e5fe4877a834767e1f291be8f6527da056
D-u-c-k-s-e-l <98615937+D-u-c-k-s-e-l@users.noreply.github.com>
2025-03-14 14:14:21 -0500

    add fancy messages

--- setup.bash
+++ setup.bash
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+echo -e "\e[32m|>\e[m TASK: Create venv"
+echo -e "\e[32m :\e[m Creating venv at \e[35m.venv\e[0m ..."
 python3 -m venv .venv
+echo -e "\e[32m :\e[m Activating venv ..."
 source .venv/bin/activate
+echo -e "\e[32m<|\e[m TASK COMPLETE"
 
+echo -e "\e[32m|>\e[m TASK: Run updates"
 ./update.bash
+echo -e "\e[32m<|\e[m TASK COMPLETE"
 
+
+echo -e "\e[32m|>\e[m TASK: Create links"
+echo -e "\e[32m :\e[m Linking new file \e[35mapp\e[m to \e[35mapp.py\e[m ..."
 ln -s app.py app
+echo -e "\e[32m :\e[m Linking new file \e[35m/home/protected/run-site.bash\e[m to \e[35mrun.bash\e[m ..."
 ln -s run.bash /home/protected/run-site.bash
+echo -e "\e[32m<|\e[m TASK COMPLETE"
--- update.bash
+++ update.bash
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+echo -e "\e[32m >\e[m update script \e32m< \e[m"
+
+echo -e "\e[32m :\e[m pulling ..."
 git pull
+echo -e "\e[32m :\e[m getting requirements ..."
 pip install -r requirements.txt
+echo -e "\e[32m :\e[m ensuring pip is up-to-date ..."
+pip install --upgrade pip
+
+echo -e "\e[32m >\e[m update script done \e32m< \e[m"
